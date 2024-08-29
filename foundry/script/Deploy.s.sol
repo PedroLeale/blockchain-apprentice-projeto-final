4de24e049e1fff96eb5752605b81725df437c3ac
Pedro <pedroleale@gmail.com>
2024-08-29 16:55:13 -0300

    Script de deploy

--- foundry/script/Deploy.s.sol
+++ foundry/script/Deploy.s.sol
@@ -2,7 +2,20 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
+import {PharmacyDAO} from "src/PharmacyDAO.sol";
+import {Prescription} from "src/Prescription.sol";
 
 contract DeployScript is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        
+        vm.startBroadcast(deployerPrivateKey);
+        PharmacyDAO pharmacyDAO = new PharmacyDAO("CryptoPharma");
+        Prescription prescription = new Prescription(address(pharmacyDAO));
+        pharmacyDAO.setPrescriptionToken(address(prescription));
+        vm.stopBroadcast();
 
+        console.log("PharmacyDAO deployed at address: ", address(pharmacyDAO));
+        console.log("Prescription deployed at address: ", address(prescription));
+    }
 }
