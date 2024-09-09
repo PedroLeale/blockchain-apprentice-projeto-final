9f210072e2ac807600e9ef8500cc9c014190302c
Pedro <pedroleale@gmail.com>
2024-09-09 17:12:58 -0300

    fix: amplify variaveis de ambiente

--- frontend/src/components/MintPrescriptionForm.js
+++ frontend/src/components/MintPrescriptionForm.js
@@ -60,7 +60,7 @@ const MintPrescriptionForm = () => {
 
     try {
       const response = await axios.put(
-        `${process.env.BACKEND_URL}/mint-prescription`,
+        `${process.env.REACT_BACKEND_URL}/mint-prescription`,
         {
           key: `prescriptions/${prescription.prescriptionId}.json`,
           expires: 300,
