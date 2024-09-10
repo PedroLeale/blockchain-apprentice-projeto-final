6044f343199e2d0d03d5495bc3bd1e6ef74399d8
Pedro Henrique Rossetto Natal <pedronatalr@gmail.com>
2024-09-10 13:00:59 -0300

    fix coments

--- backend/cors.js
+++ backend/cors.js
@@ -5,7 +5,6 @@ module.exports = (req, res, next) => {
   res.set('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS')
   res.set('Access-Control-Allow-Headers', 'Content-Type,Authorization')
 
-  // handle preflight request
   if (req.method === 'OPTIONS') {
     res.set('Content-Length', '0')
     return res.status(204).send()
